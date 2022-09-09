8ab378147e9d971524f1728dcb0a390446c8e3d4
Kyle Bauer <kbau121@gmail.com>
2022-09-09 13:28:10 -0400

    Updated "color.h" for reusability of code

--- PathTracer/PathTracer.cpp
+++ PathTracer/PathTracer.cpp
@@ -38,7 +38,7 @@ int main() {
 	// Image Settings
 
 	const double aspect_ratio = 16.0 / 9.0;
-	const int image_width = 1024;
+	const int image_width = 256;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 	const int image_channels = 3;
 	const int image_data_stride = image_width * image_channels;
@@ -81,7 +81,7 @@ int main() {
 				pixel_color += ray_color(r, world, max_depth);
 			}
 
-			write_color(data[ind], pixel_color, samples_per_pixel);
+			write_color(&data[ind], pixel_color, samples_per_pixel);
 			ind += image_channels;
 		}
 	}
--- PathTracer/color.h
+++ PathTracer/color.h
@@ -4,23 +4,9 @@
 
 #include <iostream>
 
-void write_color(std::ostream& out, color pixel_color, int samples_per_pixel) {
-	double r = pixel_color.x();
-	double g = pixel_color.y();
-	double b = pixel_color.z();
+int* get_RGB(color pixel_color, int samples_per_pixel) {
+	int RGB[3];
 
-	// Scale based on the number of samples and apply gamma correction (gamma=2)
-	double scale = 1.0 / samples_per_pixel;
-	r = sqrt(scale * r);
-	g = sqrt(scale * g);
-	b = sqrt(scale * b);
-
-	out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
-		<< static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
-		<< static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
-}
-
-void write_color(unsigned char& data, color pixel_color, int samples_per_pixel) {
 	double r = pixel_color.x();
 	double g = pixel_color.y();
 	double b = pixel_color.z();
@@ -31,7 +17,25 @@ void write_color(unsigned char& data, color pixel_color, int samples_per_pixel) 
 	g = sqrt(scale * g);
 	b = sqrt(scale * b);
 
-	(&data)[0] = unsigned char(256 * clamp(r, 0.0, 0.999));
-	(&data)[1] = unsigned char(256 * clamp(g, 0.0, 0.999));
-	(&data)[2] = unsigned char(256 * clamp(b, 0.0, 0.999));
+	RGB[0] = unsigned char(256 * clamp(r, 0.0, 0.999));
+	RGB[1] = unsigned char(256 * clamp(g, 0.0, 0.999));
+	RGB[2] = unsigned char(256 * clamp(b, 0.0, 0.999));
+
+	return RGB;
+}
+
+void write_color(std::ostream& out, color pixel_color, int samples_per_pixel) {
+	int* RGB = get_RGB(pixel_color, samples_per_pixel);
+
+	out << RGB[0] << ' '
+		<< RGB[1] << ' '
+		<< RGB[2] << '\n';
+}
+
+void write_color(unsigned char* data, color pixel_color, int samples_per_pixel) {
+	int* RGB = get_RGB(pixel_color, samples_per_pixel);
+
+	data[0] = RGB[0];
+	data[1] = RGB[1];
+	data[2] = RGB[2];
 }
